2cc9f736b978a5b3228e8b4c3eec693683ec9ab6
Yamin <yamine@shelfy.io>
2021-01-05 20:05:54 +0200

    Add unit test to the workflow

--- app/src/androidTest/java/com/example/ciapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/ciapplication/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.ciapplication", appContext.packageName)
+        assertEquals("com.example.ciapplication2", appContext.packageName)
     }
 }
